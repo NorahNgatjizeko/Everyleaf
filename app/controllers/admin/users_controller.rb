64f17e765af88cb6c4970a5a8754c7c75c33dd48
norah ngatjizeko <tjeripo86@live.com>
2021-07-14 19:41:30 +0900

    admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,13 @@
 class Admin::UsersController < ApplicationController
-  skip_before_action :login_required, only: [:edit, :update, :destroy, :show], raise: false
-	before_action :is_admin, only: [:edit, :update, :destroy, :show,:index, :new]
+    before_action :login_required, only: [:edit, :update, :destroy, :show, :index]
+	before_action :is_admin
 	before_action :set_user, only: [:edit, :update, :destroy, :show]
-
   def index
 		@users = User.select(:id, :name, :admin)
 	end
-
 	def new
 		@user = User.new
 	end
-
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -19,10 +16,8 @@ class Admin::UsersController < ApplicationController
 		  	render :new
 		end
 	end
-
 	def edit
 	end
-
 	def update
 		if params[:admin]
 			if @user.admin == true
@@ -32,34 +27,34 @@ class Admin::UsersController < ApplicationController
 			end
 		else
 			if @user.update(user_params)
-			    redirect_to admin_users_path, notice: "Profile edited!"
+			    redirect_to admin_users_path, notice: "Profil edited!"
 			else
 			    render :edit
 			end
 		end
 	end
-
 	def show
 	end
-
 	def destroy
-		@user.destroy
-		redirect_to admin_users_path, notice:"User deleted!"
+		user = User.find(params[:id])
+		if (current_user == user) && (current_user.admin?)
+			redirect_to admin_users_path, notice: "You cannot delete own admin account!"
+		elsif @user.destroy
+			redirect_to admin_users_path, notice: "user deleted!"
+		else
+			redirect_to admin_users_path, notice: "user not deleted!"
+        end
 	end
-
 	private
-
 	def user_params
 		params.require(:user).permit(:name, :email, :password,
-	                          	 :password_confirmation, :admin)
+	                          	 :password_confirmation)
 	end
-
 	def set_user
 		@user = User.find(params[:id])
 	end
-
 	def is_admin
-		if current_user.admin == false || current_user.admin.blank?
+		if current_user.present? && current_user.admin == nil
 			redirect_to tasks_path, notice:"Only administrators can access this page!!"
 		end
 	end
